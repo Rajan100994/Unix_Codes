d09fd77ab7e01b7cae33338fe7ec99599ba00582
Rajan100994 <rajanjjhaveri@gmail.com>
2017-07-19 00:29:00 -0500

    Codes for all assignments of CS 5375- Priciples of Unix

--- forkprac/fork_1.c
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-#include<sys/types.h>
-#include<unistd.h>
-int main(){
-fork(); 
-printf("This is process %d \n",getpid());
-return 0;
-}
--- forkprac/fork_2.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-#include<sys/types.h>
-#include<unistd.h>
-int main(){
-fork(); 
-fork(); 
-printf("This is process %d \n",getpid());
-return 0;
-}
